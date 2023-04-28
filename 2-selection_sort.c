a1a78b3935dd4aee9fc8b2eccb4757c4833fb534
Peter Mgembe <peterrobsson@gmail.com>
2023-04-28 12:22:14 -0700

    fixed the selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,23 +8,33 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t current_min, x, sub_x;
-	int swap;
+	int sorted_index, minimum_index, i;
+	int swap_value, swapped;
 
-	for (x = 0; x < size && size > 2; x++)
+
+	sorted_index = -1;
+
+	while (sorted_index <= (int) (size - 1))
 	{
-		current_min = x;
-		for (sub_x = x + 1; sub_x < size; sub_x++)
+		/* set the initial minimum index*/
+		minimum_index = sorted_index + 1;
+		swapped = 0;
+
+		for (i = sorted_index + 1; i < (int) size; i++)
 		{
-			if (array[sub_x] < array[current_min])
+			/* if the iterated value is less than the value at the min index */
+			if (array[i] < array[minimum_index])
 			{
-				current_min = sub_x;
+				minimum_index = i;
+				swapped = 1;
 			}
 		}
-		swap = array[x];
-		if (x > 0)
+		swap_value = array[sorted_index + 1];
+		array[sorted_index + 1] = array[minimum_index];
+		array[minimum_index] = swap_value;
+		if (swapped)
 			print_array(array, size);
-		array[x] = array[current_min];
-		array[current_min] = swap;
+
+		sorted_index++;
 	}
 }
